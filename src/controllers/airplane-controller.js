100191ac2b04265e1f70967227638c220a2d172e
TusharChoudharykp <tusharkumarchoudhary8@gmail.com>
2025-03-28 14:46:06 +0530

    Added get all airplanes api

--- src/controllers/airplane-controller.js
+++ src/controllers/airplane-controller.js
@@ -2,28 +2,35 @@ const { StatusCodes } = require("http-status-codes");
 
 const { AirplaneService } = require("../services");
 
+const ErrorResponse = require("../utils/common/error-response");
+const SuccessResponse = require("../utils/common/success-response");
+
 async function createAirplane(req, res) {
   try {
     const airplane = await AirplaneService.createAirplane({
       modelNumber: req.body.modelNumber,
       capacity: req.body.capacity,
     });
-    return res.status(StatusCodes.CREATED).json({
-      success: true,
-      message: "Successfully create an airplane",
-      data: airplane,
-      error: {},
-    });
+    SuccessResponse.data = airplane;
+    return res.status(StatusCodes.CREATED).json(SuccessResponse);
   } catch (error) {
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      success: false,
-      message: "Something went wrong while creating airplane",
-      data: {},
-      error: error,
-    });
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
+}
+
+async function getAirplanes(req, res) {
+  try {
+    const airplanes = await AirplaneService.getAirplanes();
+    SuccessResponse.data = airplanes;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
   }
 }
 
 module.exports = {
   createAirplane,
+  getAirplanes,
 };
---
+++ src/seeders/20250328073907-add-airplanes.js
@@ -0,0 +1,42 @@
+"use strict";
+const { Op } = require("sequelize");
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert("Airplanes", [
+      {
+        modelNumber: "Air Asia",
+        capacity: 800,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        modelNumber: "SpiceJet",
+        capacity: 200,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Airplanes", {
+      [Op.or]: [{ modelNumber: "Air Asia" }, { modelNumber: "SpiceJet" }],
+    });
+  },
+};
