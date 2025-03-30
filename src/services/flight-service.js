023544ad3b8f399a14a1bfd61df8a0d27bdf2340
TusharChoudharykp <tusharkumarchoudhary8@gmail.com>
2025-03-30 11:54:13 +0530

    Added flights all functionalities

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -1,4 +1,5 @@
 const { StatusCodes } = require("http-status-codes");
+const { Op } = require("sequelize");
 
 const { FlightRepository } = require("../repositories");
 const AppError = require("../utils/errors/app-error");
@@ -26,6 +27,43 @@ async function createFlight(data) {
   }
 }
 
+async function getAllFlights(query) {
+  let customFilter = {};
+  //const endingTripTime = "23:59:00";
+
+  if (query.trips) {
+    [departureAirportId, arrivalAirportId] = query.trips.split("-");
+    customFilter.departureAirportId = departureAirportId;
+    customFilter.arrivalAirportId = arrivalAirportId;
+  }
+  if (query.price) {
+    [minPrice, maxPrice] = query.price.split("-");
+    customFilter.price = {
+      [Op.between]: [minPrice, maxPrice == undefined ? 25000 : maxPrice],
+    };
+  }
+  if (query.travellers) {
+    customFilter.totalSeats = {
+      [Op.gte]: query.travellers,
+    };
+  }
+  if (query.tripDate) {
+    customFilter.departureTime = {
+      [Op.gte]: query.tripDate,
+    };
+  }
+  try {
+    const flights = await flightRepository.getAllFlights(customFilter);
+    return flights;
+  } catch (error) {
+    throw new AppError(
+      "Cannot fetch data of all the flights",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
 module.exports = {
   createFlight,
+  getAllFlights,
 };
